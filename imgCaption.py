abbb86abda889717bbc8a3d59212ef1a6f1added
Fan <59141023+fan19-hub@users.noreply.github.com>
2023-11-27 18:57:49 -0600

    Update imgCaption.py

--- imgCaption.py
+++ imgCaption.py
@@ -7,73 +7,73 @@ client = OpenAI()
 
 def img2txt(url):
   print("Generating Image Captioning...")
-  response = client.chat.completions.create(
-    model="gpt-4-vision-preview",
-    messages=[
-      {
-        "role": "user",
-        "content": [
-          {"type": "text", "text": "Let's think step by step. First, extract all the text in the image (OCR). Then, describe this picture. If it is a photo, how many characters are in this pic? what are they doing? what are their relations. what is the background? what activity is this? If it is a chart, what kind of chart it is? what interesting data does it have? what is the title or purpose of this chart? use 200 words to answer"},
+  if "http:" in url:
+      response = client.chat.completions.create(
+        model="gpt-4-vision-preview",
+        messages=[
           {
-            "type": "image_url",
-            "image_url": {
-              "url": f"{url}",
-            },
-          },
+            "role": "user",
+            "content": [
+              {"type": "text", "text": "Let's think step by step. First, extract all the text in the image (OCR). Then, describe this picture. If it is a photo, how many characters are in this pic? what are they doing? what are their relations. what is the background? what activity is this? If it is a chart, what kind of chart it is? what interesting data does it have? what is the title or purpose of this chart? use 200 words to answer"},
+              {
+                "type": "image_url",
+                "image_url": {
+                  "url": f"{url}",
+                },
+              },
+            ],
+          }
         ],
+        max_tokens=300,
+      )
+      return response.choices[0].message.content
+  else:
+      import base64
+      import requests
+
+      # OpenAI API Key
+      api_key = os.getenv("OPENAI_API_KEY")
+
+      # Function to encode the image
+      def encode_image(image_path):
+        with open(image_path, "rb") as image_file:
+          return base64.b64encode(image_file.read()).decode('utf-8')
+
+      # Path to your image
+      image_path = url
+
+      # Getting the base64 string
+      base64_image = encode_image(image_path)
+
+      headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {api_key}"
       }
-    ],
-    max_tokens=300,
-  )
-  return response.choices[0].message.content
+
+      payload = {
+        "model": "gpt-4-vision-preview",
+        "messages": [
+          {
+            "role": "user",
+            "content": [
+              {
+                "type": "text",
+                "text": "Let's think step by step. First, extract all the text in the image (OCR). Then, describe this picture. If it is a photo, how many characters are in this pic? what are they doing? what are their relations. what is the background? what activity is this? If it is a chart, what kind of chart it is? what interesting data does it have? what is the title or purpose of this chart? use 200 words to answer"
+              },
+              {
+                "type": "image_url",
+                "image_url": {
+                  "url": f"data:image/jpeg;base64,{base64_image}"
+                }
+              }
+            ]
+          }
+        ],
+        "max_tokens": 300
+      }
+
+      response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+
+      return eval(response.text)["choices"][0]["message"]["content"]
 
 
-
-
-# import base64
-# import requests
-
-# # OpenAI API Key
-# api_key = "YOUR_OPENAI_API_KEY"
-
-# # Function to encode the image
-# def encode_image(image_path):
-#   with open(image_path, "rb") as image_file:
-#     return base64.b64encode(image_file.read()).decode('utf-8')
-
-# # Path to your image
-# image_path = "path_to_your_image.jpg"
-
-# # Getting the base64 string
-# base64_image = encode_image(image_path)
-
-# headers = {
-#   "Content-Type": "application/json",
-#   "Authorization": f"Bearer {api_key}"
-# }
-
-# payload = {
-#   "model": "gpt-4-vision-preview",
-#   "messages": [
-#     {
-#       "role": "user",
-#       "content": [
-#         {
-#           "type": "text",
-#           "text": "What’s in this image?"
-#         },
-#         {
-#           "type": "image_url",
-#           "image_url": {
-#             "url": f"data:image/jpeg;base64,{base64_image}"
-#           }
-#         }
-#       ]
-#     }
-#   ],
-#   "max_tokens": 300
-# }
-
-# response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
-
-# print(response.json())
