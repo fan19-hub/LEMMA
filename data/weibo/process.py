030eef5dbdedaf1a0aeb61f2c2fb050958c5fd2e
keyangds <107345948+keyangds@users.noreply.github.com>
2024-01-27 22:57:36 -0600

    new

--- data/weibo/process.py
+++ data/weibo/process.py
@@ -1,22 +1,23 @@
 import json
+import random
 
-# Assuming your JSON file is named 'data.json' with a different encoding
+# Assuming your JSON file is named 'data.json'
 input_json_file = 'data/weibo/weibo.json'
-output_json_file = 'data/weibo/weibo_2.json'
+output_json_file = 'data/weibo/weibo_shuffled.json'
 
-# Read the JSON file with the existing encoding
-with open(input_json_file, 'r') as file:
+# Read the JSON file
+with open(input_json_file, 'r', encoding='utf-8') as file:
     data = json.load(file)
 
-processed_data = []
+# Shuffle the data
+random.shuffle(data)
 
-for element in data:
-    element['image'] = 'weibo/' + element['image'] + '.jpg'
-    processed_data.append(element)
-  
-# Save the filtered data to a new JSON file
+# Save the shuffled data to a new JSON file
 with open(output_json_file, 'w', encoding='utf-8') as file:
-    json.dump(processed_data, file, ensure_ascii=False, indent=2)
+    json.dump(data, file, ensure_ascii=False, indent=2)
+
+print(f'Shuffled data has been saved to {output_json_file}')
+
       
 
     
