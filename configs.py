7c3fe32aa45c90fd9e1da03c365cfd75efa6bb95
Fan <59141023+fan19-hub@users.noreply.github.com>
2024-06-14 03:25:28 +0800

    cache bug fix

--- configs.py
+++ configs.py
@@ -4,6 +4,8 @@ data_root="data/"
 scripts_root="scripts"
 out_root="out/"
 cache_root="data/cache/"
+if not os.path.exists(cache_root):
+    os.makedirs(cache_root)
 imgbed_root="https://raw.githubusercontent.com/fan19-hub/LEMMA/main/data/"
 OPENAI_KEY = os.getenv("OPENAI_API_KEY")
 definition_path = "prompts/definition.json"
